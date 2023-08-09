57ed0a39147d7bda11a4285d88d0c049cdea48a0
Siddique305 <140276626+Siddique305@users.noreply.github.com>
2023-08-10 00:46:12 +0530

    Update XMLTestResultsToExcel.java
    Additional code to get the description as well for the Test Case. And shorting the test case ID

--- demo/src/main/java/com/example/demo/XMLTestResultsToExcel.java
+++ demo/src/main/java/com/example/demo/XMLTestResultsToExcel.java
@@ -9,11 +9,15 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 public class XMLTestResultsToExcel{
     public static void main(String[] args) {
@@ -28,6 +32,13 @@ public class XMLTestResultsToExcel{
 
     private static List<TestResult> parseXMLTestResults(String xmlFilePath) {
         List<TestResult> testResults = new ArrayList<>();
+        
+        //To get the description from reading the Excel file (By defining Path)
+//        Map<String, String> testDescriptions = getTestDescriptions();
+        
+        //To get the Description from TestDescription class
+        TestDescription td = new TestDescription();
+        Map<String, String> testDescriptions = td.getTestDescriptions();
 
         try {
             File xmlFile = new File(xmlFilePath);
@@ -47,8 +58,8 @@ public class XMLTestResultsToExcel{
                 // Extract Test Case Id and Status from the line content
                 String testCaseId = extractTestCaseId(lineContent);
                 TestStatus status = extractTestStatus(lineContent);
-
-                testResults.add(new TestResult(testCaseId, status));
+                String description = testDescriptions.getOrDefault(testCaseId, "Unknown description");
+                testResults.add(new TestResult(testCaseId, status, description));
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -59,13 +70,14 @@ public class XMLTestResultsToExcel{
 
     private static String extractTestCaseId(String lineContent) {
         //As the Test Case Id is enclosed in square brackets.
-        int startIdx = lineContent.indexOf('[');
-        int endIdx = lineContent.indexOf(']', startIdx);
+    	int startIdx = lineContent.indexOf("TSQA_");
+        int endIdx = lineContent.indexOf("]", startIdx);
         if (startIdx != -1 && endIdx != -1) {
-            return lineContent.substring(startIdx + 1, endIdx);
+            return lineContent.substring(startIdx, endIdx);
         } else {
             return "Unknown";
         }
+        
     }
 
     private static TestStatus extractTestStatus(String lineContent) {
@@ -79,7 +91,30 @@ public class XMLTestResultsToExcel{
             return TestStatus.UNKNOWN;
         }
     }
-
+    
+//  --To below code is to get Description from Excel---  
+//    private static Map<String, String> getTestDescriptions() {
+//        Map<String, String> testDescriptions = new HashMap<>();
+//
+//        try (FileInputStream fis = new FileInputStream("E:/Siddiq/Data Excel/DescriptionOfTest.xlsx");  //Seting up the path of excel
+//             Workbook workbook = new XSSFWorkbook(fis)) {
+//            Sheet sheet = workbook.getSheetAt(0);
+//
+//            for (int rowIdx = 1; rowIdx <= sheet.getLastRowNum(); rowIdx++) {
+//                Row row = sheet.getRow(rowIdx);
+//                if (row != null) {
+//                    String testCaseId = row.getCell(0).getStringCellValue();
+//                    String description = row.getCell(1).getStringCellValue();
+//                    testDescriptions.put(testCaseId, description);
+//                }
+//            }
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return testDescriptions;
+//    }
+    
     private static void writeToExcel(String excelFilePath, List<TestResult> testResults) {
         try (Workbook workbook = new XSSFWorkbook()) {
             Sheet sheet = workbook.createSheet("Test Results");
@@ -89,12 +124,14 @@ public class XMLTestResultsToExcel{
             Row headerRow = sheet.createRow(rowIdx++);
             headerRow.createCell(0).setCellValue("Test Case Id");
             headerRow.createCell(1).setCellValue("Status");
+            headerRow.createCell(2).setCellValue("Description");
 
             // Fill data rows
             for (TestResult testResult : testResults) {
                 Row dataRow = sheet.createRow(rowIdx++);
                 dataRow.createCell(0).setCellValue(testResult.getTestCaseId());
                 dataRow.createCell(1).setCellValue(testResult.getStatus().toString());
+                dataRow.createCell(2).setCellValue(testResult.getDescription());
             }
 
             // Save the workbook to the file
@@ -111,8 +148,11 @@ public class XMLTestResultsToExcel{
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH.mm.ss");
         String timestamp = dateFormat.format(date);
-        return "XMLToExcelfile_" + timestamp + ".xlsx";
+        return "XMLtoExcelFile_" + timestamp + ".xlsx";
     }
+    
+   
+
 }
 
 enum TestStatus {
@@ -125,10 +165,12 @@ enum TestStatus {
 class TestResult {
     private String testCaseId;
     private TestStatus status;
+    private String description;
 
-    public TestResult(String testCaseId, TestStatus status) {
+    public TestResult(String testCaseId, TestStatus status, String description) {
         this.testCaseId = testCaseId;
         this.status = status;
+        this.description = description;
     }
 
     public String getTestCaseId() {
@@ -138,5 +180,14 @@ class TestResult {
     public TestStatus getStatus() {
         return status;
     }
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	} 
+      
 }
 
